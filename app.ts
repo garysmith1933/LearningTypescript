19012773fe29407b071e05f1841b2696d2568aee
Gary Smith <garysmith193358@gmail.com>
2022-11-02 10:36:07 -0400

    reviewed type castingand Discriminating Unions

--- app.ts
+++ app.ts
@@ -1,20 +1,20 @@
 type Admin = {
-    name:string;
-    privileges: string[];
+  name:string;
+  privileges: string[];
 };
 
 type Employee = {
-    name:string;
-    startDate: Date;
+name:string;
+  startDate: Date;
 }
 
 //interface ElevatedEmployee extends Employee, Admin
 type ElevatedEmployee = Admin & Employee
 
 const e1: ElevatedEmployee = {
-    name: 'Gary',
-    privileges: ['create-server'],
-    startDate: new Date()
+  name: 'Gary',
+  privileges: ['create-server'],
+  startDate: new Date()
 }
 
 type Combinable = string | number;
@@ -23,42 +23,42 @@ type Numeric = number | boolean;
 type Universal = Combinable & Numeric;
 
 function add1(a:Combinable, b: Combinable) {
-    if(typeof a === 'string' || typeof b === 'string') {
-        return a.toString() + b.toString();
-    }
-    return a + b
+  if(typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString();
+  }
+  return a + b
 }
 
 type UnknownEmployee = Employee | Admin;
 
 function printEmployeeInformation(emp: UnknownEmployee) {
-    console.log(`Name ${emp.name}`)
-    //type guard
-    if('privileges' in emp) {
-        console.log( `Privleges ${emp.privileges}`)
-    }
+  console.log(`Name ${emp.name}`)
+  //type guard example
+  if('privileges' in emp) {
+    console.log( `Privleges ${emp.privileges}`)
+  }
 
-    if('startDate' in emp) {
-        console.log(`Start Date ${emp.startDate}`)
-    }  
+  if('startDate' in emp) {
+    console.log(`Start Date ${emp.startDate}`)
+  }  
 }
 
 printEmployeeInformation(e1)
 
 class Car {
-    drive() {
-        console.log('Driving...')
-    }
+  drive() {
+    console.log('Driving...')
+  }
 }
 
 class Truck {
-    drive() {
-        console.log('Driving a truck')
-    }
+  drive() {
+      console.log('Driving a truck')
+  }
 
-    loadCargo(amount:number) {
-        console.log(`Loading cargo... ${amount}`)
-    }
+  loadCargo(amount:number) {
+    console.log(`Loading cargo... ${amount}`)
+  }
 }
 
 type Vehicle = Car | Truck
@@ -67,10 +67,55 @@ const v1 = new Car();
 const v2 = new Truck();
 
 function useVehicle(vehicle: Vehicle) {
-    vehicle.drive();
-    if (vehicle instanceof Truck) {
-      vehicle.loadCargo(1000)
-    }
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000)
+  }
 }
 
+useVehicle(v1)
 useVehicle(v2)
+
+//Type guarding
+//for objects we can use instanceof or in
+//for other types we can use typeof
+
+//Discrimated Unions
+interface Bird {
+  //literal type
+  type: 'bird';
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: 'horse';
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse
+
+function moveAnimal(animal: Animal) {
+    let speed;
+    switch(animal.type) {
+      case 'bird':
+        speed = animal.flyingSpeed;
+        break;
+      case 'horse': 
+        speed = animal.runningSpeed;
+        break;
+    }
+
+    console.log(`Moving with speed ${speed}`)
+}
+
+moveAnimal({type:'bird', flyingSpeed: 10})
+
+//Type Casting
+
+// const userInputElement = <HTMLInputElement>document.getElementById('user-input')!
+
+const userInputElement = <HTMLInputElement>document.getElementById('user-input')
+
+if(userInputElement){
+  (userInputElement as HTMLInputElement).value = 'Hi there!'
+}
